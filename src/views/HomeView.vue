f1b20f2c356c79a3e39ad4cfb4ae846dd83f7a20
CatBoxy <catboxy@gmail.com>
2024-09-07 01:13:19 +0800

    nav 項目垂直對齊完成

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -33,54 +33,53 @@
 <template>
   <div class="index">
     <div class="container">
-      <div class="row justify-content-center">
+      <div class="row">
         <!-- 左側選單 -->
         <div class="col-3">
+          <a class="navbar-brand" href="#">
+            <img src="../assets/images/logo.svg" alt="">
+          </a>
+
           <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link navbar-brand" href="#">
-                <img src="../assets/images/logo.svg" alt="">
-              </a>
-            </li>
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">
                 <span class="material-icons">assessment</span>
-                Dashboard
+                <span class="nav-title">Dashboard</span>
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">
                 <span class="material-icons">question_answer</span>
-                Question
+                <div class="nav-title">Question</div>
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">
                 <span class="material-icons">assignment</span>
-                Assignment
+                <div class="nav-title">Assignment</div>
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">
                 <span class="material-icons">video_library</span>
-                Course
+                <div class="nav-title">Course</div>
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">
                 <span class="material-icons">people</span>
-                Admin
+                <div class="nav-title">Admin</div>
               </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">
                 <span class="material-icons">settings</span>
-                Settings
+                <div class="nav-title">Settings</div>
               </a>
             </li>
             <!-- 底部登入者頭像 -->
             <li class="nav-item">
-              <a class="nav-link" href="#" style="display: inline-block;">
+              <a class="nav-link" href="#">
                 <div class="d-flex justify-content-center">
                   <div class="avatar">
                     <img src="../assets/images/andychen.jpeg" alt="" srcset="">
